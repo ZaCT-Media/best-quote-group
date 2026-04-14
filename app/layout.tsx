e6ae833dafbc205b947cbca0ee46cf3eb0172629
Cole <cole10.taylor@gmail.com>
2026-04-14 17:07:19 +0100

    added facebook pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,6 +35,9 @@ export default function RootLayout({
         {/* Google Tag Manager */}
         <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-WM225N64');` }} />
         {/* End Google Tag Manager */}
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','807045789783610');fbq('track','PageView');` }} />
+        {/* End Meta Pixel Code */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}
@@ -42,6 +45,10 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WM225N64" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe></noscript>
         {/* End Google Tag Manager (noscript) */}
+        {/* Meta Pixel (noscript) */}
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <noscript><img height="1" width="1" style={{ display: 'none' }} src="https://www.facebook.com/tr?id=807045789783610&ev=PageView&noscript=1" alt="" /></noscript>
+        {/* End Meta Pixel (noscript) */}
         {children}
         <WhatsAppButton />
       </body>
